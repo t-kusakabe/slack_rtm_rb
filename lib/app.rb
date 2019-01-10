39bcf1997650a76ff1caf450231bb3f003bef21b
t-kusakabe <ttmac0704@gmail.com>
2019-01-10 19:19:55 +0900

    Create slack rtm script

--- lib/app.rb
+++ lib/app.rb
@@ -1,1 +1,18 @@
-puts 'hello world'
+require 'dotenv'
+require 'slack'
+
+Dotenv.load
+
+client = Slack::Client.new token: ENV['SLACK_TOKEN']
+rtm = client.realtime
+rtm.on :message do |message|
+  p message
+  if message['text'] == 'ping'
+    slack_client.chat_postMessage(
+      channel: message['channel'],
+      text: 'pong'
+    )
+  end
+end
+
+rtm.start
